b4fce6a88081293b892104d9ce0a83fa109d016d
Gavin Taylor <taylor@usna.edu>
2018-03-08 09:40:20 -0600

    working interface example

--- network/trivMPIControl.py
+++ network/trivMPIControl.py
@@ -1,7 +1,21 @@
 from mpi4py import MPI
+import numpy as np
 
 comm=MPI.COMM_WORLD
 rank=comm.Get_rank()
 size=comm.Get_size()
 
 print "python "+str(rank)+" of "+str(size)
+
+status=MPI.Status()
+state=np.zeros((2,))
+comm.Recv(state,source=0,status=status)
+
+while (status.Get_tag() != 2):
+  print "got state ",state
+  #feed state into NN, get back an action, for now, do this:
+  action=np.random.rand(2)
+  comm.Send(action,dest=0,tag=1)
+  comm.Recv(state,source=0,status=status)
+
+print "Python closing"
--- network/trivMPISim.cpp
+++ network/trivMPISim.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <stdlib.h>
 #include "mpi.h"
 using namespace std;
 
 int main(int argc, char* argv[]) {
+
+  int NUMRUNS=5;
+
   int rank;
   int size;
   MPI_Init(NULL,NULL);
@@ -10,6 +14,21 @@ int main(int argc, char* argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD,&size);
 
   cout << "C++ " << rank << " of " << size << endl;
+
+  for (int i = 0; i < NUMRUNS; i++) {
+    double state[2];
+    state[0]=double(rand()); //instead of this, you would run your simulator
+    state[1]=double(rand()); //to assign these values
+    cout << "Sending " << state[0] << ' ' << state[1] << endl;
+
+    MPI_Send(state,2,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
+    double action[2];
+    MPI_Recv(action,2,MPI_DOUBLE,1,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+  }
+
+  MPI_Send(NULL,0,MPI_INT,1,2,MPI_COMM_WORLD);
+
+  cout << "C++ closing" << endl;
 
   MPI_Finalize();
 }
