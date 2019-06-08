96672c872384e57da78d1ab9c2a51997424a5bcb
Chesley Elizabeth Krug <m203330@hokulea02.mhpcc.hpc.mil>
2019-06-07 14:03:33 -1000

    hi

--- learner/evaluate.py
+++ learner/evaluate.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import h5py
 import glob
+import ntpath
 from baselines import logger
 from collections import deque
 from baselines.ppo2.runner import Runner
@@ -26,7 +27,7 @@ if __name__ == '__main__':
   lam=0.99
   jobnumber=os.getenv("PBS_JOBID").split('.')[0]
   workdir=os.getenv("WORKDIR")
-  loglist=glob.glob(workdir, +'/autovalves/learner/logs/*')
+  loglist=glob.glob(workdir+"/autovalves/learner/logs/*")
   fcount = len(loglist)
   startIndex = rank*(fcount//size)+min(rank,(fcount%size)) 
   endIndex = ((rank+1)*(fcount//size))+min(rank,(fcount%size))
@@ -37,13 +38,12 @@ if __name__ == '__main__':
   
   logger.configure(dir=workdir+"/autovalves/learner/evaluate_logs", format_strs=['stdout', 'log'], log_suffix=jobnumber)
   parser=argparse.ArgumentParser() 
-  parser.add_argument('rank',help='rank of log file')
-  parser.add_argument('jobnm', help='jobnumber of log file')
   parser.add_argument('numtrue', help='number of completed runs')
   args=parser.parse_args()
 
  #with open(workdir+"/autovalves/learner/logs/log"+args.jobnm+"-rank00"+args.rank+".txt") as f:
   for name in loglist:
+    print("New log file: ", name)
     with open(name) as f:
       for line in f:
         if "Number" in line and "Layers" in line:
@@ -58,7 +58,7 @@ if __name__ == '__main__':
         if "Value" in line:
           vf_coef=line.split()[-1] 
           vf_coef=float(vf_coef)
-
+    print("made it here")
     env = DummyVecEnv([lambda:ChemicalEnv(comm)])
     env = VecNormalize(env)
     network = "mlp"
@@ -71,10 +71,15 @@ if __name__ == '__main__':
     nbatch_train = nbatch // nminibatches
     max_grad_norm=0.5
     policy=build_policy(env,network, num_layers=num_layers, num_hidden=layer_width)
+    print("made it there")
     model = model_fn(policy=policy, ob_space=ob_space, ac_space=ac_space, nbatch_act=nevs, nbatch_train=nbatch_train, nsteps=nsteps, ent_coef=ent_coef, vf_coef=vf_coef, max_grad_norm=max_grad_norm)
-    mname= name[3:].split(.)
-    tmp = mname[0][-1]    
-    model.load(mname[0])
+    mname= (ntpath.basename(name)[3:]).split('.')
+    split_k = mname[0].split('k')
+    num = str(int(split_k[1]))         
+    con_name = split_k[0]+'k'+num
+    print("Model Name: ", con_name)      
+        
+    model.load(workdir+"/autovalves/learner/models/"+con_name)
     eval_runner=Runner(env=env, model=model, nsteps=nsteps, gamma=gamma, lam=lam)
     count = 0
     obs, returns, masks, actions, values, neglogpacs, states, epinfos = None, None, None, None, None, None,None, None 
@@ -82,6 +87,7 @@ if __name__ == '__main__':
     f = h5py.File(workdir+"/autovalves/learner/hdf5/"+mname[0]+".hdf5","w")
 
     while(count < int(args.numtrue)):
+      print("We evaluatin")
       eval_obs, eval_returns, eval_masks, eval_actions, eval_values, eval_neglogpacs, eval_states, eval_epinfos =eval_runner.run()
 
       for index,i in enumerate(eval_masks):
