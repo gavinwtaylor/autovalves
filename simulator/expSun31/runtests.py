aa05ee2d592985d62b9e4e692ab0b804ee1efda4
Patrick James Bishop <m210480@hokulea02.mhpcc.hpc.mil>
2019-06-11 11:09:42 -1000

    w/ mult evs

--- simulator/expSun31/runtests.py
+++ simulator/expSun31/runtests.py
@@ -2,7 +2,7 @@ import itertools
 import subprocess
 
 lrs = [1e-2, 1e-4, 1e-6]
-numiters = [1000000]
+numiters = [5000]
 entropy = [0]   
 value = [0.5]
 layers = [2]
@@ -11,8 +11,8 @@ envs = [5]
 
 def createScript(combos):
     cmd = 'mpiexec -np ' + str(len(combos)) 
-    cmd2 = ' ./simulator/expSun31/envSim : -np ' + str(len(combos))
-    cmd3 = ' python learner/ex.py'
+    cmd2 = ' ./simulator/expSun31/multEnvSim : -np ' + str(len(combos))
+    cmd3 = ' python learner/multEx.py'
 
     rateList  = [str(combo[0]) for combo in combos]
     tstepList = [str(combo[1]) for combo in combos]
@@ -38,7 +38,7 @@ def createScript(combos):
     #PBS -l select=1:ncpus=30
     #PBS -A MHPCC38870258
     #PBS -q standard
-    #PBS -l walltime=4:00:00
+    #PBS -l walltime=36:00:00
 
     cd $WORKDIR/autovalves
     """ + cmd
