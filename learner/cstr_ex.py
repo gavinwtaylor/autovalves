a89fd971aa761f869e2446e1ef1ffdea3d9e06a0
Chesley Elizabeth Krug <m203330@hokulea02.mhpcc.hpc.mil>
2019-06-13 15:40:14 -1000

    hi

--- learner/cstr_ex.py
+++ learner/cstr_ex.py
@@ -9,7 +9,7 @@ import numpy as np
 from cstrEnv import CSTREnvironment
 from baselines import bench, logger
 
-def train(lrnrt, timest, entr, valcoef, numlyrs, lyrsize, jobnumber):
+def train(lrnrt, timest, entr, valcoef, numlyrs, lyrsize, jobnumber, numevs):
     from baselines.common.vec_env.vec_normalize import VecNormalize
     from baselines.common import set_global_seeds
     from baselines.bench import Monitor
@@ -20,51 +20,42 @@ def train(lrnrt, timest, entr, valcoef, numlyrs, lyrsize, jobnumber):
     from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
     import multiprocessing
    
- 
-    ncpu = 1
+    print("Print 1") 
+    ncpu = 10
     config = tf.ConfigProto(allow_soft_placement=True,
                             intra_op_parallelism_threads=ncpu,
                             inter_op_parallelism_threads=ncpu)
     tf.Session(config=config).__enter__()
-
-    env = DummyVecEnv([lambda:CSTREnvironment()])
+    print("Print 2")
+    env = DummyVecEnv([lambda:CSTREnvironment() for i in range(numevs)])
 
     env = VecNormalize(env)
     policy = "mlp"
+    print("Print 3")
     model = ppo2.learn(network=policy, env=env,total_timesteps=timest,ent_coef=entr,lr=lrnrt,vf_coef=valcoef,log_interval=10, num_layers=numlyrs, num_hidden=lyrsize)
     model.save(workdir+"/autovalves/learner/models/"+str(jobnumber))
-  
+    print("Print 4")
     return model, env
 
 if __name__ == '__main__':
     parser=argparse.ArgumentParser()
-    parser.add_argument('lrs',help='comma-separated list of learning rates')
-    parser.add_argument('tss', help='comma-separated list of timesteps')
-    parser.add_argument('entps',help='comma-separated list of entropies')
-    parser.add_argument('vcfs',help='comma-separated list of value coefficents')
-    parser.add_argument('nlyrs', help='comma-separated list of layer numbers')
-    parser.add_argument('slyrs', help='comma-separated list of layer sizes')
+    parser.add_argument('lrs',help='learning rate',type=float)
+    parser.add_argument('tss', help='timestep', type=int)
+    parser.add_argument('entps',help='entropy', type=float)
+    parser.add_argument('vcfs',help='value coefficent',type=float)
+    parser.add_argument('nlyrs', help='layer numbers', type=int)
+    parser.add_argument('slyrs', help='layer size',type=int) 
+    parser.add_argument('nevs', help='list of environments',type=int)
     args=parser.parse_args()
-    lrs=[float(lr) for lr in args.lrs.split(',')]
-    tss=[int(ts) for ts in args.tss.split(',')]
-    entps=[float(ent) for ent in args.entps.split(',')]
-    vcfs=[float(vcf) for vcf in args.vcfs.split(',')]
-    nlyrs=[int(lyr) for lyr in args.nlyrs.split(',')]
-    slyrs=[int(sz) for sz in args.slyrs.split(',')]
     workdir=os.getenv("WORKDIR")
     jobnumber=os.getenv("PBS_JOBID").split('.')[0]
     logger.configure(dir=workdir+"/autovalves/learner/logs", format_strs=['stdout','log'], log_suffix=jobnumber)
     logger.log("Job Number: ",jobnumber)
-    #logger.log("Rank: ",rank)
-    partner=0
-    rank=8
-    logger.log("Learning Rate: ", lrs[partner])
-    logger.log("Timestep: ", tss[partner])
-    logger.log("Entropy: ", entps[partner])
-    logger.log("Value Coefficent: ",vcfs[partner])
-    logger.log("Number of Layers: ", nlyrs[partner])
-    logger.log("Width of Layers: ", slyrs[partner])
-    train(lrs[partner], tss[partner], entps[partner], vcfs[partner], nlyrs[partner], slyrs[partner],jobnumber)
-    temp = np.array([0,1])
-    temp = temp.astype(float)
+    logger.log("Learning Rate: ", args.lrs)
+    logger.log("Timestep: ", args.tss)
+    logger.log("Entropy: ", args.entps)
+    logger.log("Value Coefficent: ",args.vcfs)
+    logger.log("Number of Layers: ", args.nlyrs)
+    logger.log("Width of Layers: ", args.slyrs)
+    train(args.lrs, args.tss, args.entps, args.vcfs, args.nlyrs, args.slyrs,jobnumber,args.nevs)
       
