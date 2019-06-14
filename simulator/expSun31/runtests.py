13b73899724d6b4768420ddade8cd066b2f0e094
Patrick James Bishop <m210480@hokulea02.mhpcc.hpc.mil>
2019-06-14 11:18:59 -1000

    comments

--- simulator/expSun31/runtests.py
+++ simulator/expSun31/runtests.py
@@ -1,14 +1,14 @@
 import itertools
 import subprocess
 
-lrs = [1e-6, 1e-8, 1e-10]
-numiters = [5000000000]
-entropy = [0]   
-value = [0.5]
-layers = [3]
-perc = [64]
-envs = [10]
-numNodes = 3
+lrs = [1e-6, 1e-8, 1e-10]  #learning rate
+numiters = [5000000000]    #timesteps
+entropy = [0]              #value for randomness
+value = [0.5]              #value coefficient
+layers = [3]               #number of layers within neural net
+perc = [64]                #width of neural net
+envs = [10]                #number of environments
+numNodes = 3               #number of nodes
 
 def createScript(combos):
     cmd = 'mpiexec -np ' + str(4*numNodes) + ' python learner/cstr_ex.py ' 
@@ -31,8 +31,11 @@ def createScript(combos):
       #qsub the script for each combo
       subprocess.run(['qsub'],input=script,encoding='ascii')
 
+
+#create every combination for the parameters above
 combinations=[combo for combo in itertools.product(lrs,numiters,entropy, value,layers,perc, envs)]
 while len(combinations) > 0: 
+    #assign four per node 
     createScript(combinations[-4:])
     for i in range( min(4,len(combinations)) ):
         combinations.pop()
