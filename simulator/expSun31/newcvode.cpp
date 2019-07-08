9bc459f5c6a435be541a656efc0054199eee213b
Gavin Taylor <taylor@usna.edu>
2019-07-08 10:40:05 -0400

    newcvode

--- simulator/expSun31/newcvode.cpp
+++ simulator/expSun31/newcvode.cpp
@@ -167,6 +167,7 @@ int main(void) {
 
     // initialize x in a circle surroudning the region of interest; 
     rad = (double(p)/double(NRuns)) * 2.0 * M_PI;
+    rad=2;
     NV_Ith_S(x, 0)   = 0.55 + x0scale * cos(rad); // mol/m3
     NV_Ith_S(x, 1)   = 375 + x1scale * sin(rad); // deg K
 
